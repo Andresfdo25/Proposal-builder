5f6fecab6747b154339c9dded79517fa6a5227fa
AndresFdo <afcg.250@gmail.com>
2025-08-10 22:53:46 -0500

    chore: add .gitignore and initial scaffold

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-indigo-600">
+        Tailwind v4 on Vite ✅
+      </h1>
+      <p className="mt-2 text-sm text-gray-600">
+        Si esto se ve con estilos, Tailwind está funcionando.
+      </p>
+    </div>
+  );
+}
